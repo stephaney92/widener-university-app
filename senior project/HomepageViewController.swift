d398153333249d648e72064c3cb4d7f57c120612
stephaney sumner <stephaneysumner@gmail.com>
2020-01-28 13:28:01 -0500

    added logout button added navigation to send it back to root view homepage

--- senior project/HomepageViewController.swift
+++ senior project/HomepageViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class HomepageViewController: UIViewController {
 
@@ -14,11 +15,30 @@ class HomepageViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        //hides back button on homepage 
+        //navigationItem.hidesBackButton = true
     }
     
-
+    @IBAction func LogoutPressed(_ sender: UIBarButtonItem) {
+        let firebaseAuth = Auth.auth()
+        do {
+            //pops all view off the stack except the root view and takes you back to root view loginpage
+            try firebaseAuth.signOut()
+            navigationController?.popToRootViewController(animated: true)
+            
+            //catch blocked triggered if there are any issues signing out user
+        } catch let signOutError as NSError {
+            print ("Error signing out: %@", signOutError)
+        }
+    }
+    
+    
+    
+    
+        
+     }
     /*
-    // MARK: - Navigation
+     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -27,4 +47,4 @@ class HomepageViewController: UIViewController {
     }
     */
 
-}
+
