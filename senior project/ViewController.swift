050fa0c780835ad974cdd8be1f13ef7873443a17
stephaney sumner <stephaneysumner@gmail.com>
2020-01-27 20:01:44 -0500

    initial commit

---
+++ senior project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  senior project
+//
+//  Created by Stephaney Sumner on 1/27/20.
+//  Copyright © 2020 senior project. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
