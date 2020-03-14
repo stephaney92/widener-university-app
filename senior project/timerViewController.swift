8e0d3dc40feff0ae5a201e975493a5c6d12ae0f5
stephaney sumner <stephaneysumner@gmail.com>
2020-03-13 20:43:12 -0400

    add buttons to timer page, added timer view controller

---
+++ senior project/timerViewController.swift
@@ -0,0 +1,36 @@
+//
+//  timerViewController.swift
+//  senior project
+//
+//  Created by Stephaney Sumner on 3/13/20.
+//  Copyright © 2020 senior project. All rights reserved.
+//
+
+import UIKit
+
+class timerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func startButtonPressed(_ sender: Any) {
+    }
+    @IBAction func stopButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func addButtonPressed(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
