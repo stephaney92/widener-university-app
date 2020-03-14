a7d872fa9a15e8872365c00303ea1992c1e4b2ea
stephaney sumner <stephaneysumner@gmail.com>
2020-03-13 21:19:54 -0400

    added seconds portion of timer

--- senior project/timerViewController.swift
+++ senior project/timerViewController.swift
@@ -11,19 +11,43 @@ import UIKit
 class timerViewController: UIViewController {
 
    @IBOutlet weak var timerLabel: UILabel!
-      
+    var seconds = 60 //This variable will hold a starting value of seconds. It could be any amount above 0.
+    var timer = Timer()
+    var isTimerRunning = false //This will be used to make sure only one timer is created at a time.
+    var resumeTapped = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func startButtonPressed(_ sender: Any) {
+    @IBAction func startButtonPressed(_ sender: UIButton) {
+        runTimer()
     }
-    @IBAction func stopButtonPressed(_ sender: Any) {
+    //timeInterval - how often the a method will be called, selector - method being called updateTimer
+    func runTimer() {
+         timer = Timer.scheduledTimer(timeInterval: 1, target: self,   selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
-    
-    @IBAction func addButtonPressed(_ sender: Any) {
+    @objc func updateTimer() {
+        seconds -= 1     //This will decrement(count down)the seconds.
+        timerLabel.text = "\(seconds)" //This will update the label.
+    }
+    //The first time the stop button is tapped the resumeTapped will be false and the timer will be stopped with timer.invalidate(), else The following time the pause button is tapped runTimer() will again initialize the timer
+    @IBAction func stopButtonPressed(_ sender: UIButton) {
+        if self.resumeTapped == false {
+                 timer.invalidate()
+                 self.resumeTapped = true
+            } else {
+                 runTimer()
+                 self.resumeTapped = false
+            }
+    }
+    //stops timer and resets the clock back to zero
+    @IBAction func addButtonPressed(_ sender: UIButton) {
+        timer.invalidate()
+        seconds = 60
+        timerLabel.text = "\(seconds)"
     }
     /*
     // MARK: - Navigation
